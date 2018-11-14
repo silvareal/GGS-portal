eceb43364b786821ee4986610d833e07de735140
sylvernus <sylvernusakubo@yahoo.com>
2018-11-14 15:42:34 +0100

    db url

--- school/settings.py
+++ school/settings.py
@@ -177,3 +177,7 @@ REST_FRAMEWORK = {
 }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
