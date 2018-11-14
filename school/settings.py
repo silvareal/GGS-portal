570514df7cff0b98759a614431547f9a660fdc8c
sylvernus <sylvernusakubo@yahoo.com>
2018-11-14 15:32:31 +0100

    pip heroku update

--- school/settings.py
+++ school/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -175,4 +176,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 50,
 }
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
